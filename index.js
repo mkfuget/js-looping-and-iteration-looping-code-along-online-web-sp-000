338e12956631ac079ca4ddcbda1f80d17952e16c
Michael Fuget <michaelfuget@gmail.com>
2021-02-01 09:57:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, event)
+{
+  var out = []
+  for(let i=0; i<array.length; i++)
+  {
+    out.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+  }
+  return out
+}
+function countDown(num)
+{
+  for(let j = num; j>= 0; j--)
+  {
+    console.log(j)
+  }
+}
