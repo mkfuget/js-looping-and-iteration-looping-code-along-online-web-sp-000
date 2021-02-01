8e9bcc03d87c28f8ed7ee4381a8b18e391966ec3
Michael Fuget <michaelfuget@gmail.com>
2021-02-01 09:52:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+function writeCards(array, event)
+{
+  out = []
+  for(let i=0; i<array.length; i++)
+  {
+    out.push("Thank you, ${array[i]}, for the wonderful ${event} gift!")
+  }
+}  
